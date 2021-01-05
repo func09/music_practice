232e4ec69891123033013245698607240ef820c0
Mitsuru Haga <haga@rollcake.co>
2021-01-05 13:06:12 +0900

    Fix Scale

--- src/views/Practice04.vue
+++ src/views/Practice04.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="Plactice01">
     <h1 class="ui header dividing">Scales</h1>
+<<<<<<< Updated upstream
     <table class="ui table celled fixed definition unstackable">
       <thead>
         <tr class="center aligned">
@@ -16,6 +17,44 @@
           <td>{{ item.intervals.join(", ") }}</td>
           <td>{{ item.notes.join(", ") }}</td>
           <td><Player v-bind:score="getScore(item)"></Player></td>
+=======
+
+    <table
+      class="ui table celled small compact"
+      v-for="scaleName in scaleNames"
+      v-bind:key="scaleName"
+    >
+      <caption class="ui header">
+        {{
+          scaleName
+        }}
+      </caption>
+      <thead>
+        <tr
+          class="ui center aligned"
+          v-for="scale in getScales(scaleName, notes).slice(0, 1)"
+          v-bind:key="scale.name"
+        >
+          <th v-for="interval in scale.intervals" v-bind:key="interval">
+            <span class="ui label" v-bind:class="getBadgeStyle(interval)">
+              {{ interval }}
+            </span>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          class="ui center aligned"
+          v-for="scale in getScales(scaleName, notes)"
+          v-bind:key="scale.name"
+        >
+          <td
+            v-for="(interval, index) in scale.intervals"
+            v-bind:key="interval"
+          >
+            {{ pitchName(simplifyNote(scale.notes[index])) }}
+          </td>
+>>>>>>> Stashed changes
         </tr>
       </tbody>
     </table>
@@ -38,6 +77,7 @@ const defaultNotes = Range.chromatic(["C3", "C4"], { sharps: true });
 
 const store = {
   state: {
+<<<<<<< Updated upstream
     key: defaultKey,
     scales: defaultScales
       .map((scale) => {
@@ -46,6 +86,22 @@ const store = {
         });
       })
       .flat(),
+=======
+    notes: Range.chromatic(["C3", "B3"], { sharps: true }),
+    scaleNames: [
+      "major",
+      "minor",
+      "melodic minor",
+      "harmonic minor",
+      "pentatonic",
+      "minor pentatonic",
+      "minor #7M pentatonic",
+      "blues",
+      "spanish heptatonic",
+      "minor six diminished",
+      "dominant diminished",
+    ],
+>>>>>>> Stashed changes
   },
 };
 
@@ -63,8 +119,8 @@ export default {
       return store.state.scales;
     },
   },
-  components: {
-    Player,
-  },
+  // components: {
+  //   Player,
+  // },
 };
 </script>
